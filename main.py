c663be9f266dec541e2768d4198383ec7f52799d
Garrit Schroeder <garritsidney.schroeder@ottogroup.com>
2021-01-02 19:31:30 +0100

    get start and end position of movies

--- main.py
+++ main.py
@@ -49,35 +49,50 @@ def create_video_fingerprint(path):
     return video_fingerprint
 
 
-def get_unique_frames_fingerprint(video_fingerprints):
-    unique_frames = []
-    for video_fingerprint in video_fingerprints:
-        for frame_fingerprint in re.findall('................', video_fingerprint):
-            unique_frames.append(frame_fingerprint)
-    unique_frames = list(set(unique_frames))
-    return unique_frames
-
-
-def create_matrix(unique_frames):
-    unique_frame_tokens = {}
-    for i in range(62, len(unique_frames)):
-        unique_frame_tokens[unique_frames[i]] = chr(i)
-    return unique_frame_tokens
+def get_equal_frames(print1, print2):
+    equal_frames = []
+    count = 0
+    while min(len(print1), len(print2)) > count:
+        if print1[count] == print2[count]:
+            equal_frames.append(print1[count])
+        count += 1
+    return equal_frames
 
 
 def tokenize_fingerprints(video_fingerprints):
-    unique_frames = get_unique_frames_fingerprint(video_fingerprints)
-    frame_matrix = create_matrix(unique_frames)
-    token_video_fingerprints = []
-    for video_fingerprint in video_fingerprints:
-        token_video_fingerprint = ""
-        for frame_fingerprint in re.findall('................', video_fingerprint):
-            try:
-                token_video_fingerprint += frame_matrix[frame_fingerprint]
-            except Exception as e:
-                print(e)
-        token_video_fingerprints.append(token_video_fingerprint)
-    pass
+    unique_prints = set()
+    for fingerprint in video_fingerprints:
+        for frame_print in re.findall("................", fingerprint):
+            unique_prints.add(frame_print)
+    matrix = {}
+    counter = 60
+    for unique_print in unique_prints:
+        matrix[unique_print] = chr(counter)
+        counter += 1
+    tokenprints = []
+    for fingerprint in video_fingerprints:
+        t = fingerprint
+        for key in matrix.items():
+            t = t.replace(key[0], str(key[1]))
+        tokenprints.append(t.replace(".", ""))
+    return tokenprints, matrix
+
+
+def get_start_end(print1, print2):
+    offset = len(print1)
+    highest_equal_frames = []
+    for i in range(0, offset):
+        equal_frames = get_equal_frames(print1[-i:], print2)
+        if len(equal_frames) > len(highest_equal_frames):
+            highest_equal_frames = equal_frames
+    for i in range(0, offset):
+        equal_frames = get_equal_frames(print1, print2[i:])
+        if len(equal_frames) > len(highest_equal_frames):
+            highest_equal_frames = equal_frames
+    p = re.compile(".*?".join(highest_equal_frames))
+    search = re.search(p, print1)
+    search2 = re.search(p, print2)
+    return (search.start(), search.end()), (search2.start(), search2.end())
 
 
 def for_files(files):
@@ -88,11 +103,14 @@ def for_files(files):
             with open("frames/" + replace(file) + "/aa_fingerprint.txt", "r") as text_file:
                 fingerprint = text_file.read()
         else:
-            print(file + " fingerprint does not exist - loading it")
+            print(file + " fingerprint does not exist - creating it")
             fingerprint = create_video_fingerprint(file)
             write_fingerprint(file, fingerprint)
         fingerprints.append(fingerprint)
-    tokenize_fingerprints(fingerprints)
+    tokens, matrix = tokenize_fingerprints(fingerprints)
+    print(files[0] + " - " + str(get_start_end(tokens[0], tokens[1])))
+    print(files[0] + " - " + str(get_start_end(tokens[2], tokens[3])))
+    print(files[0] + " - " + str(get_start_end(tokens[4], tokens[5])))
 
 
 print(datetime.now())
@@ -101,5 +119,4 @@ sample_frame = 1  # take every X frame
 paths = [
 ]
 for_files(paths)
-
 print(datetime.now())
