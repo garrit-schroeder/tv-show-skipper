3442785ca7dfe6a2e42da128780403784ed3ecc1
cameron <cameronlambert98@gmail.com>
2022-03-18 02:10:28 -0700

    can query all tv episodes in jellyfin

--- jellyfin_queries.py
+++ jellyfin_queries.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 def map_path(path, path_map):
     new_path = path
@@ -24,11 +25,53 @@ def get_shows(client = None, path_map = []):
                 'Fields': (
                     "ProviderIds",
                     "Path"
-                )
+                ),
+                #'Limit': 1
             })
+
     if 'Items' in result and result['Items']:
         for item in result['Items']:
             print(item['Name'])
             print(item['Id'])
-            print(map_path(item['Path'], path_map))
-        print(len(result['Items']))
+            if 'Path' in item:
+                print(map_path(item['Path'], path_map))
+            get_seasons(client, path_map, item['Id'])
+            sleep(0.2)
+        print('found %s shows' % len(result['Items']))
+
+def get_seasons(client = None, path_map = [], seriesID = ''):
+    if client == None or seriesID == '':
+        return False
+
+    result = client.jellyfin.get_seasons(seriesID)
+
+    if 'Items' in result and result['Items']:
+        for item in result['Items']:
+            print(item['Name'])
+            print(item['Id'])
+            if 'Path' in item:
+                print(map_path(item['Path'], path_map))
+            get_season(client, path_map, seriesID, item['Id'])
+            sleep(0.2)
+        print('found %s seasons' % len(result['Items']))
+
+def get_season(client = None, path_map = [], seriesID = '', seasonID = ''):
+    if client == None or seriesID == '' or seasonID == '':
+        return False
+
+    result = client.jellyfin.shows("/%s/Episodes" % seriesID, {
+            'UserId': "{UserId}",
+            'SeasonId': seasonID,
+            'Fields': (
+                "ProviderIds",
+                "Path"
+            )
+        })
+    
+    if 'Items' in result and result['Items']:
+        for item in result['Items']:
+            print(item['Name'])
+            print(item['Id'])
+            if 'Path' in item:
+                print(map_path(item['Path'], path_map))
+        print('found %s episodes' % len(result['Items']))
