10a0db54f7dba9414393aeefaeb9873b3e3465ac
cameron <cameronlambert98@gmail.com>
2022-03-18 00:49:51 -0700

    support path mapping for jellyfin docker

--- jellyfin.py
+++ jellyfin.py
@@ -8,15 +8,29 @@ server_url = os.environ['JELLYFIN_URL']
 server_username = os.environ['JELLYFIN_USERNAME']
 server_password = os.environ['JELLYFIN_PASSWORD']
 
+def get_path_map():
+    path_map = []
+    with open('path_map.txt', 'r') as file:
+        for line in file:
+            if line.startswith('#'):
+                continue
+            map = line.strip().split(':')
+            if len(map) != 2:
+                continue
+            path_map.append((map[0], map[1]))
+    return path_map
+
 def parse_jellyfin_shows():
     if server_url == '' or server_username == '' or server_password == '':
         print('missing server info')
         return
-    client = jellyfin_login(server_url, server_username, server_password)
+    path_map = get_path_map()
 
-    jellyfin_queries.get_shows(client)
+    client = jellyfin_login(server_url, server_username, server_password)
+    jellyfin_queries.get_shows(client, path_map)
 
     jellyfin_logout()
+    print(path_map)
 
 if __name__ == '__main__':
     parse_jellyfin_shows()
--- jellyfin_queries.py
+++ jellyfin_queries.py
@@ -1,4 +1,16 @@
-def get_shows(client = None, path_mapping = []):
+import os
+
+def map_path(path, path_map):
+    new_path = path
+
+    for mapping in path_map:
+        if mapping[1] in path:
+            remainder = path[len(mapping[1]) + 1:] if str(path[len(mapping[1]):]).startswith('/') else path[len(mapping[1]):]
+            new_path = os.path.join(mapping[0], remainder)
+            break
+    return new_path
+
+def get_shows(client = None, path_map = []):
     if client == None:
         return False
 
@@ -17,5 +29,6 @@ def get_shows(client = None, path_mapping = []):
     if 'Items' in result and result['Items']:
         for item in result['Items']:
             print(item['Name'])
-            print(item['Path'])
+            print(item['Id'])
+            print(map_path(item['Path'], path_map))
         print(len(result['Items']))
